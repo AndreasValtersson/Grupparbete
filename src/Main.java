d8a943fb125c3ac013e9006f45ca49aa89371ee5
Andreas Valtersson <Andreas_valter@hotmail.com>
2021-06-21 13:47:57 +0200

    Menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the worlds best calculator!");
+        System.out.print("Enter enter a number: ");
+        int i = scanner.nextInt();
+        System.out.print("Please enter a second number: ");
+        int j = scanner.nextInt();
+        System.out.println("Please press a number to navigate");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.print("Option: ");
+        int input = scanner.nextInt();
+        scanner.next();
+
+        switch (input){
+            case 1:
+                addition(i,j);
+                break;
+            case 2:
+                subtraction(i,j);
+                break;
+            case 3:
+                multiplcation(i,j);
+                break;
+            case 4:
+                division(i,j);
+                break;
+            default:
+                System.out.println("Not a valid number");
+        }
     }
 }
