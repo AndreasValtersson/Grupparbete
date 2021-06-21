c6aeb5d15737bfc08a1a2aff0d65fb0d9233871e
Filip Garcia <mailafilip@gmail.com>
2021-06-21 13:39:54 +0200

    Addition and subtraction methods

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+    }
+
+    public static int addition(int i, int j){
+        return i + j;
+    }
+
+    public static int subtraction(int i, int j){
+        return i - j;
     }
 }
